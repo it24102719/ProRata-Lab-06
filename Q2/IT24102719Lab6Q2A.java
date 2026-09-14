e290c41d0267d11402fec0dc1f2aa8432298e48f
it24102719 <it24102719@my.sliit.lk>
2026-09-14 21:01:26 +0530

    Added Lab6Q2A.javaLab6Q2A

---
+++ Q2/IT24102719Lab6Q2A.java
@@ -0,0 +1,12 @@
+public class IT24102719Lab6Q2A {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+
+        System.out.println();
+    }
+}
